94f4c33aded2b89619c27cb1495df807c05c1da0
GeraldaGultom <geraldagultom@gmail.com>
2024-12-04 16:10:20 +0700

    Kami telah selesai tugas

--- X02.java
+++ X02.java
@@ -1,2 +1,51 @@
-// NIM - Your Name
-// NIM - Your Name
+// 12S24044 - DIANITA LORENSIA BR GINTING
+// 12S24051 - GERALDA NATALI GULTOM
+import java.util.*;
+import java.lang.Math;
+
+public class T06 {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String[] name = new String[4];
+        int i;
+
+        i = 0;
+        initialized(name, i);
+        var_input(name);
+        output(name);
+    }
+    
+    public static void initialized(String[] name, int i) {
+        int initialized;
+
+        for (i = 0; i <= 3; i++) {
+            name[i] = "";
+        }
+    }
+    
+    public static void var_input(String[] name) {
+        String var_input;
+        int i;
+
+        for (i = 0; i <= 3; i++) {
+            name[i] = input.nextLine();
+            if (name[i].equals("---")) {
+                i = 9;
+            }
+        }
+    }
+    
+    public static void output(String[] name) {
+        String output;
+        int i;
+
+        for (i = 0; i <= 3; i++) {
+            if (name[i].equals("---")) {
+                i = 72;
+            } else {
+                System.out.println(name[i]);
+            }
+        }
+    }
+}
